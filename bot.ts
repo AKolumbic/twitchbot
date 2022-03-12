8a6c7d9194591d90cf07ca060e438b8c0452f6e7
AKolumbic <andrewkolumbic@gmail.com>
2022-03-11 17:11:40 -0800

    working chatbot

--- bot.ts
+++ bot.ts
@@ -1,24 +1,23 @@
 // require('dotenv').config();
 import dotenv from 'dotenv';
-import tmi, { Options } from 'tmi.js';
+import tmi, { Options, Userstate, Client } from 'tmi.js';
 import { executeCommand } from './utilities/execute-command';
 
 // Configure Environment Variables
 dotenv.config();
 
+const channel = 'drosshole';
 // Define configuration options
 const opts: Options = {
   identity: {
     username: process.env.BOT_USERNAME,
     password: process.env.PASSWORD
   },
-  channels: [
-    'drosshole'
-  ]
+  channels: [ channel ]
 };
 
 // Create a client with our options
-const client = new tmi.client(opts);
+const client: Client = new tmi.client(opts);
 // Register our event handlers (defined below)
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
@@ -26,19 +25,17 @@ client.on('connected', onConnectedHandler);
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler(target: string, context: any, msg: string, self: any) {
-  console.log('Message Handler Args: ', { target, context, msg, self });
-  // Log context
-  console.log('onMessageHandler context: ', context);
+function onMessageHandler(target: string, userstate: Userstate, msg: string, self: boolean) {
+  console.log('Message Handler Args: ', { target, userstate, msg, self });
   // Ignore messages from the bot
   if (self) { return; }
   // Remove whitespace from chat message
   const commandName = msg.trim();
   // If the command is known, let's execute it
-  executeCommand(client, commandName, target);
+  executeCommand(client, commandName, target, channel);
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr: string, port: number) {
-  console.log(`* Connected to ${addr}:${port}`);
+  console.log(`* Connected to ${addr} on port:${port}`);
 }
--- utilities/execute-command.ts
+++ utilities/execute-command.ts
@@ -1,24 +1,31 @@
+import { Client } from 'tmi.js';
 import { rollDice, determineCampaign, sayMyName } from "./index";
 
 export function executeCommand(
-  client: any,
+  client: Client,
   commandName: string,
-  target: string
+  target: string,
+  channel: string
 ): void {
   sayMyName('Heisenberg');
   switch (commandName.toLocaleLowerCase()) {
     case '!help':
-      client.say()
+      client.say(channel, '')
+      break;
     case '!roll':
       const roll = rollDice();
-      client.say(`${target} rolled a ${roll}`);
+      client.say(channel, `${target} rolled a ${roll}`);
+      break;
     case '!campaign':
       const campaignText = determineCampaign();
-      client.say(campaignText);
+      client.say(channel, campaignText);
+      break;
     case '!lurk':
       console.log(`${target} is lurking...`);
+      break;
     case '!twitter' || '!youtube' || '!instagram' || '!discord':
-      client.say(`Hit me up @drosshole on ${commandName.substring(1)}`)
+      client.say(channel, `Hit me up @drosshole on ${commandName.substring(1)}`)
+      break;
     default:
       console.log(`* Unknown command ${commandName}`);
   }
