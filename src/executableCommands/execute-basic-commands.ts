c9ce10f376d41aa062487e6a8985577c5db3a158
AKolumbic <andrewkolumbic@gmail.com>
2022-04-06 13:47:05 -0700

    updates info command

--- src/executableCommands/execute-basic-commands.ts
+++ src/executableCommands/execute-basic-commands.ts
@@ -1,4 +1,4 @@
-import { Client, Userstate } from 'tmi.js';
+import { Client } from 'tmi.js';
 import { username } from '../../secrets';
 import { getCampaignDescription, getCharacterSheet, getPartyMembers } from '../utilities';
 
@@ -12,13 +12,11 @@ export function executeBasicCommands(
     case '!info':
       if (chatter === `@${username}`) {
         chatbot.say(channel,
-          `DROSSBOT: Thanks for watching! You can interact with the channel by using commands like !roll, !campaign, or !character sheet.
-            If you'd like to know more, type !commands in chat!`
+          `DROSSBOT: Thanks for watching! You can interact with the channel by using commands like !roll, !campaign, or !character sheet. If you'd like to know more, type !commands in chat!`
         )
       } else {
         chatbot.say(channel,
-          `DROSSBOT: Thanks for watching, ${chatter}! You can interact with the channel by using commands like !roll, !campaign, or !character sheet.
-            If you'd like to know more, type !commands in chat!`
+          `DROSSBOT: Thanks for watching, ${chatter}! Cause of the nature of D&D, I don't interact with chat as much as other streamers, BUT I do have a chatbot if you want to get in on the action. You can interact with the channel by using commands like !roll, !campaign, or !character sheet. If you'd like to know more, type !commands in chat!`
         )
       }
       break;
@@ -47,23 +45,23 @@ export function executeBasicCommands(
 
     case '!party':
       const party = getPartyMembers();
-      chatbot.say(channel, `${party}. Type !<character name> in chat to get a link to individual character's DnD Beyond character sheet.`);
+      chatbot.say(channel, `DROSSBOT: ${party}. Type !<character name> in chat to get a link to individual character's DnD Beyond character sheet.`);
       break;
 
     case '!secrets':
-      chatbot.say(channel, `Clever, ${chatter}. Try typing 'never gonna give you up' in chat... or !wow `);
+      chatbot.say(channel, `DROSSBOT: Clever, ${chatter}. Try typing 'never gonna give you up' in chat... or !wow `);
 
     case `!discord`:
-      chatbot.say(channel, `https://discord.gg/W4VhAuU9rx`);
+      chatbot.say(channel, `DROSSBOT: https://discord.gg/W4VhAuU9rx`);
 
     case `!tip`:
-      chatbot.say(channel, `https://streamlabs.com/drosshole/tip`);
+      chatbot.say(channel, `DROSSBOT: https://streamlabs.com/drosshole/tip`);
 
     case `!donate`:
-      chatbot.say(channel, `https://streamlabs.com/drosshole/tip`);
+      chatbot.say(channel, `DROSSBOT: https://streamlabs.com/drosshole/tip`);
 
     case `!schedule`:
-      chatbot.say(channel, `TUESDAYS: CARRY THE KNOWLEDGE 5pm-7:30pm PST | WEDNESDAYS: A CLASH OF TWO FATES 5:30pm-8:30pm PST | SUNDAYS: THE SEA OF SORROWS 5pm-7pm PST`);
+      chatbot.say(channel, `DROSSBOT: TUESDAYS: CARRY THE KNOWLEDGE 5pm-7:30pm PST | WEDNESDAYS: A CLASH OF TWO FATES 5:30pm-8:30pm PST | SUNDAYS: THE SEA OF SORROWS 5pm-7pm PST`);
 
     default:
       chatbot.say(channel, `DROSSBOT: Sorry ${chatter}, ${command} is an invalid command`);
